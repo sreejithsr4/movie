fe075b0246ab07e14b5f435a497c9509b323bf91
SREEJITH M Methilthodi <morayursreejith@gmail.com>
2023-08-10 18:17:07 +0530

    movie1

--- mov.js
+++ mov.js
@@ -1,6 +1,6 @@
 function movieSearch() {
     movie = movie.value
-    fetch(`http://www.omdbapi.com/?t=${movie}&apikey=222e9039`).then(data => data.json()).then(movie => displayMovie(movie))
+    fetch(`http://www.omdbapi.com/?apikey=222e9039&t=${movie}`).then(data => data.json()).then(movie => displayMovie(movie))
     function displayMovie(arrayMovie) {
         console.log(arrayMovie);
         title = arrayMovie.Title
@@ -12,7 +12,7 @@ function movieSearch() {
         act=arrayMovie.Actors
         writer=arrayMovie.Writer
         result.innerHTML = `<div class="col-md-4"><h2>${title}</h2><img src="${poster}" alt=""></div>
-        <div class="col-md-8 mt-3"><p>Summary:${plot}</p> <p>Director:${dir}</p><p>Writer:${writer}</p>
+        <div class="col-md-8 mt-5"><p>Summary:${plot}</p> <p>Director:${dir}</p><p>Writer:${writer}</p>
         <p>Actors:${act}</p><p>Released Date:${date}</p>
         <p>Genre:${genre}</p></div>`
 
