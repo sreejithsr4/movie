a24eb8bb2810a22d17ca79613bb6f8c39452a4c0
SREEJITH M Methilthodi <morayursreejith@gmail.com>
2023-08-10 17:44:42 +0530

    movie

---
+++ mov.js
@@ -0,0 +1,20 @@
+function movieSearch() {
+    movie = movie.value
+    fetch(`http://www.omdbapi.com/?t=${movie}&apikey=222e9039`).then(data => data.json()).then(movie => displayMovie(movie))
+    function displayMovie(arrayMovie) {
+        console.log(arrayMovie);
+        title = arrayMovie.Title
+        poster = arrayMovie.Poster
+        plot=arrayMovie.Plot
+        date = arrayMovie.Released
+        genre=arrayMovie.Genre
+        dir=arrayMovie.Director
+        act=arrayMovie.Actors
+        writer=arrayMovie.Writer
+        result.innerHTML = `<div class="col-md-4"><h2>${title}</h2><img src="${poster}" alt=""></div>
+        <div class="col-md-8 mt-3"><p>Summary:${plot}</p> <p>Director:${dir}</p><p>Writer:${writer}</p>
+        <p>Actors:${act}</p><p>Released Date:${date}</p>
+        <p>Genre:${genre}</p></div>`
+
+    }
+}
